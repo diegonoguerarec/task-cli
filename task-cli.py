2d77c99e7a0d439dbcac9e3aee0e4ad700db6a07
Diegao <diegonoguerarec@gmail.com>
2025-04-06 20:34:09 -0300

    List functionality added

--- task-cli.py
+++ task-cli.py
@@ -25,6 +25,17 @@ def add_task(task):
     # User output
     print(f"Task added successfully (ID: {tasks["current_id"]-1})")
 
+def list_tasks(status):
+    # Read json file
+    with open("tasks.json", mode="r") as f:
+        tasks = json.load(f)
+
+    # User output
+    print(f"{"ID"[:3]:<4} {"Description"[:30]:<31} {"Status"[:11]:<12} {"Created at"[:20]:<21} {"Updated at"[:20]:<21}")
+    print("-"*92)
+    for task in tasks["tasks"]:
+        print(f"{str(task["id"])[:3]:<4} {task["description"][:30]:<31} {task["status"][:11]:<12} {str(task["createdAt"])[:20]:<21} {str(task["updatedAt"])[:20]:<21}")
+
 def main():
     # Creating json file if it does not exist
     if os.path.exists("tasks.json") == False:
@@ -40,10 +51,21 @@ def main():
     add_parser = subparsers.add_parser("add", help="Add a new task")
     add_parser.add_argument("task", type=str, help="Task description")
 
+    # list command
+    list_parser = subparsers.add_parser("list", help="List all tasks")
+    list_parser.add_argument("status",
+                             nargs="?", # Status argument is optional
+                             choices=["todo", "in-progress", "done"],
+                             type=str,
+                             help="List tasks by status")
+
     args = parser.parse_args()
 
     if args.command == "add":
         add_task(args.task)
+    
+    if args.command == "list":
+        list_tasks(args.status)
 
 if __name__ == "__main__":
     main()
